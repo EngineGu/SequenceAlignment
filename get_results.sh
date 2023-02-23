1c230b9a16defb6ff0be6e1eaa70089782b074a5
EngineGu <account_engine_gu@126.com>
2023-02-23 00:09:46 -0800

    reorganize file structure

---
+++ get_results.sh
@@ -0,0 +1,32 @@
+# use basic algorithm to process data in ./datapoints
+./basic.sh ./datapoints/in1.txt ./results/out1_basic.txt
+./basic.sh ./datapoints/in2.txt ./results/out2_basic.txt
+./basic.sh ./datapoints/in3.txt ./results/out3_basic.txt
+./basic.sh ./datapoints/in4.txt ./results/out4_basic.txt
+./basic.sh ./datapoints/in5.txt ./results/out5_basic.txt
+./basic.sh ./datapoints/in6.txt ./results/out6_basic.txt
+./basic.sh ./datapoints/in7.txt ./results/out7_basic.txt
+./basic.sh ./datapoints/in8.txt ./results/out8_basic.txt
+./basic.sh ./datapoints/in9.txt ./results/out9_basic.txt
+./basic.sh ./datapoints/in10.txt ./results/out10_basic.txt
+./basic.sh ./datapoints/in11.txt ./results/out11_basic.txt
+./basic.sh ./datapoints/in12.txt ./results/out12_basic.txt
+./basic.sh ./datapoints/in13.txt ./results/out13_basic.txt
+./basic.sh ./datapoints/in14.txt ./results/out14_basic.txt
+./basic.sh ./datapoints/in15.txt ./results/out15_basic.txt
+# use efficient algorithm to process data in ./datapoints
+./efficient.sh ./datapoints/in1.txt ./results/oute1_efficient.txt
+./efficient.sh ./datapoints/in2.txt ./results/oute2_efficient.txt
+./efficient.sh ./datapoints/in3.txt ./results/oute3_efficient.txt
+./efficient.sh ./datapoints/in4.txt ./results/oute4_efficient.txt
+./efficient.sh ./datapoints/in5.txt ./results/oute5_efficient.txt
+./efficient.sh ./datapoints/in6.txt ./results/oute6_efficient.txt
+./efficient.sh ./datapoints/in7.txt ./results/oute7_efficient.txt
+./efficient.sh ./datapoints/in8.txt ./results/oute8_efficient.txt
+./efficient.sh ./datapoints/in9.txt ./results/oute9_efficient.txt
+./efficient.sh ./datapoints/in10.txt ./results/oute10_efficient.txt
+./efficient.sh ./datapoints/in11.txt ./results/oute11_efficient.txt
+./efficient.sh ./datapoints/in12.txt ./results/oute12_efficient.txt
+./efficient.sh ./datapoints/in13.txt ./results/oute13_efficient.txt
+./efficient.sh ./datapoints/in14.txt ./results/oute14_efficient.txt
+./efficient.sh ./datapoints/in15.txt ./results/oute15_efficient.txt
